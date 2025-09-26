c35f12bb6670f91b2b89c0440197ceb91df2aeec
Lee Djen Sep <leedjensep@gmail.com>
2025-09-26 12:48:59 +0500

    INIT test task with redis

--- apps/catalog/jest.config.ts
+++ apps/catalog/jest.config.ts
@@ -10,7 +10,7 @@ const swcJestConfig = JSON.parse(
 swcJestConfig.swcrc = false;
 
 export default {
-  displayName: '@nx-microserv/products',
+  displayName: '@nx-microserv/catalog',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
