50d8de868e53b5bab52c19e59dc05623c4d0d03b
Lee Djen Sep <leedjensep@gmail.com>
2025-09-26 14:18:49 +0500

    Clear project

--- apps/api-gateway/src/app/app.controller.ts
+++ apps/api-gateway/src/app/app.controller.ts
@@ -1,22 +1,7 @@
-import { Controller, Get, Inject } from '@nestjs/common';
-import { AppService } from './app.service';
-import { ClientProxy, MessagePattern } from '@nestjs/microservices';
-import { MICROSERVICE_CLIENTS } from '../clients.enum';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(
-    @Inject(MICROSERVICE_CLIENTS.CATALOG_SERVICE)
-    private CATALOG_SERVICE: ClientProxy
-  ) {}
-
   @Get()
-  getData() {
-    const data = this.CATALOG_SERVICE.send(
-      'getData',
-      'hello world from api gateway'
-    );
-    console.log('data is ', data);
-    return data;
-  }
+  getData() {}
 }
--- apps/api-gateway/src/app/app.module.ts
+++ apps/api-gateway/src/app/app.module.ts
@@ -1,28 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { MICROSERVICE_CLIENTS } from '../clients.enum';
 
 @Module({
-  imports: [
-    ClientsModule.register([
-      {
-        name: MICROSERVICE_CLIENTS.CATALOG_SERVICE,
-        transport: Transport.TCP,
-        options: {
-          port: 3003,
-        },
-      },
-      {
-        name: MICROSERVICE_CLIENTS.PRODUCTS_SERVICE,
-        transport: Transport.TCP,
-        options: {
-          port: 3004,
-        },
-      },
-    ]),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
