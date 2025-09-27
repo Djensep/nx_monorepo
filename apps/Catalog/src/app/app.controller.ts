3c654824e5c0e54df26aa429aa876c6fc027ad89
Lee Djen Sep <leedjensep@gmail.com>
2025-09-27 18:49:14 +0500

    UPD kafka clients

--- apps/Catalog/src/app/app.controller.ts
+++ apps/Catalog/src/app/app.controller.ts
@@ -1,12 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getData() {
-    return this.appService.getData();
+  @MessagePattern('catalog.getInfo')
+  async getInfo(@Payload() dto: any) {
+    console.log("got info from catalog' + dto");
+    return 'got info from catalog' + dto;
   }
 }
--- apps/api-gateway/src/app/app.module.ts
+++ apps/api-gateway/src/app/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { MICROSERVICE_CLIENTS } from '../clients.enum';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: MICROSERVICE_CLIENTS.KAFKA_SERVICE,
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            brokers: ['localhost:9092'],
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
