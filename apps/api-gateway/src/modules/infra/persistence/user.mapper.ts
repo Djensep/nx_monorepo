f46f0237cf7c732f074266d87522b3049c99b5d3
Lee Djen Sep <leedjensep@gmail.com>
2025-09-30 01:32:59 +0500

    ADD user mapper

---
+++ apps/api-gateway/src/modules/infra/persistence/user.mapper.ts
@@ -0,0 +1,23 @@
+import { User } from '../../domain/entities/user';
+import { PasswordHash } from '../../domain/value-objects/password-hash.vo';
+import { UserEntity } from './user.entity';
+
+export class UserMapper {
+  static toDomain(row: UserEntity): User {
+    return new User(
+      row.id,
+      row.email,
+      row.name,
+      PasswordHash.fromHashed(row.passwordHash)
+    );
+  }
+
+  static toOrm(user: User): UserEntity {
+    const orm = new UserEntity();
+    orm.id = user.getId();
+    orm.email = user.getEmail();
+    orm.name = user.getName();
+    orm.passwordHash = user.getPasswordHash().unwrap();
+    return orm;
+  }
+}
