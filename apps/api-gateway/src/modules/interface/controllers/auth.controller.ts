9514ce0ae9bd9da2e33cae6b6047a14f3516bdf3
Lee Djen Sep <leedjensep@gmail.com>
2025-10-01 19:06:27 +0500

    ADD cookie setter

--- apps/api-gateway/src/modules/interface/controllers/auth.controller.ts
+++ apps/api-gateway/src/modules/interface/controllers/auth.controller.ts
@@ -1,5 +1,12 @@
-// interface/http/controllers/auth.controller.ts
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { toRegisterInput } from '../mappers/http-to-app.mapper';
 import { toRegisterResponse } from '../mappers/app-to-http.mapper';
 import { EmailAlreadyInUseError } from '../../application/errors/email-already-in-use.error';
@@ -8,20 +15,34 @@ import { RegisterRequestDto } from '../dtos/register.request.dto';
 import { LoginRequestDto } from '../dtos/login.request.dto';
 import { LoginUserUseCase } from '../../application/use-cases/login-user.use-case';
 import { InvalidCredentialsError } from '../../application/errors/invalid-credentials.error';
-
+import type { Request, Response } from 'express';
+import { LogoutUserUseCase } from '../../application/use-cases/logout-use.use-case';
+import { InvalidToken } from '../../application/errors/invalid-token.error';
 @Controller('auth')
 export class AuthController {
   constructor(
     private readonly registerUser: RegisterUserUseCase,
-    private readonly loginUser: LoginUserUseCase
+    private readonly loginUser: LoginUserUseCase,
+    private readonly logoutUser: LogoutUserUseCase
   ) {}
 
   @Post('register')
   @HttpCode(HttpStatus.CREATED)
-  async register(@Body() dtoData: RegisterRequestDto) {
+  async register(
+    @Body() dtoData: RegisterRequestDto,
+    @Res({ passthrough: true }) res: Response
+  ) {
     try {
-      const result = await this.registerUser.execute(toRegisterInput(dtoData));
-      return toRegisterResponse(result);
+      const { refreshToken, ...restData } = await this.registerUser.execute(
+        toRegisterInput(dtoData)
+      );
+      res.cookie('refreshToken', refreshToken, {
+        httpOnly: true,
+        secure: false,
+        sameSite: 'lax',
+      });
+
+      return toRegisterResponse(restData);
     } catch (e) {
       if (e instanceof EmailAlreadyInUseError) {
         throw Object.assign(new Error(e.message), { status: 409 });
@@ -31,12 +52,37 @@ export class AuthController {
   }
 
   @Post('login')
-  async login(@Body() dtoData: LoginRequestDto) {
+  async login(
+    @Body() dtoData: LoginRequestDto,
+    @Res({ passthrough: true }) res: Response
+  ) {
     try {
-      const result = await this.loginUser.execute(dtoData);
-      return toRegisterResponse(result);
+      const { refreshToken, ...restData } = await this.loginUser.execute(
+        dtoData
+      );
+      res.cookie('refreshToken', refreshToken, {
+        httpOnly: true,
+        secure: false,
+        sameSite: 'lax',
+      });
+      return toRegisterResponse(restData);
     } catch (e) {
       if (e instanceof InvalidCredentialsError) {
+        throw Object.assign(new Error(e.message), { status: 409 });
+      }
+      throw e;
+    }
+  }
+
+  @Post('logout')
+  @HttpCode(200)
+  async logout(@Req() req: Request, @Res({ passthrough: true }) res: Response) {
+    try {
+      const token = req.cookies.refreshToken;
+      await this.logoutUser.execute(token);
+      res.clearCookie('refreshToken');
+    } catch (e) {
+      if (e instanceof InvalidToken) {
         throw Object.assign(new Error(e.message), { status: 409 });
       }
       throw e;
